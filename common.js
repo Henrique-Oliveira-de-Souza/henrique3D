89595aa375e5f4766105e8e7527ea2348ca285b4
Henrique-Oliveira-de-Souza <129237173+Henrique-Oliveira-de-Souza@users.noreply.github.com>
2024-10-29 09:35:46 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -8,5 +8,11 @@ const tickConfig = {
     size: 16,
     color: getCSS('--primary-color')
 }
-
-export {getCSS, tickConfig}
+function criarGrafico(data,layout){
+    const grafico = document.createElement('div')
+        grafico.className = 'grafico'
+        document.getElementById('graficos-container')
+        .appendChild(grafico)
+        Plotly.newPlot(grafico,data,layout)
+}
+export {getCSS, tickConfig,criarGrafico}
---
+++ redesFavoritasMunddo.js
@@ -0,0 +1,8 @@
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    height:700,
+    title:{
+        
+    }
+}
