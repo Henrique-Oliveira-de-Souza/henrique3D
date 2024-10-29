bdd77b7b73d92fe9d47d615442f7e1e5b8e78bdb
Henrique-Oliveira-de-Souza <129237173+Henrique-Oliveira-de-Souza@users.noreply.github.com>
2024-10-29 12:23:52 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -15,4 +15,13 @@ function criarGrafico(data,layout){
         .appendChild(grafico)
         Plotly.newPlot(grafico,data,layout)
 }
+function incluirTexto (texto){
+    const container = document.getElementById
+    ('graficos-container')
+    const paragrafo = document.createElement('p')
+    paragrafo.classList.add('graficos-container_texto')
+    paragrafo.innerHTML = texto
+    container.appendChild(paragrafo)
+    
+}
 export {getCSS, tickConfig,criarGrafico}
